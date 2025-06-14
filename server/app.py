582450d978b5e2a19dd8bfe4c8bcacc92b8e9dbf
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-14 03:20:44 +0300

    add

--- server/app.py
+++ server/app.py
@@ -1,40 +1,48 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
+# server/app.py
+#!/usr/bin/env python3
 
-metadata = MetaData()
-db = SQLAlchemy(metadata=metadata)
+from flask import Flask, make_response
+from flask_migrate import Migrate
+
+from models import db, Earthquake
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
 db.init_app(app)
 
-class Earthquake(db.Model, SerializerMixin):
-    __tablename__ = 'earthquakes'
 
-    id = db.Column(db.Integer, primary_key=True)
-    location = db.Column(db.String(100), nullable=False)
-    magnitude = db.Column(db.Float, nullable=False)
-    year = db.Column(db.Integer, nullable=False)
+@app.route('/')
+def index():
+    body = {'message': 'Flask SQLAlchemy Lab 1'}
+    return make_response(body, 200)
 
-    def __repr__(self):
-        return f'<Earthquake {self.location} - Magnitude: {self.magnitude}, Year: {self.year}>'
-
+# Add views here
 @app.route('/earthquakes/<int:id>')
 def earthquake_by_id(id):
-    earthquake = Earthquake.query.get(id)
+    earthquake = Earthquake.query.filter_by(id = id).first()
     if earthquake:
-        return jsonify(earthquake.to_dict())
+        body = earthquake.to_dict()
+        status = 200
     else:
-        return jsonify({"message": f"Earthquake {id} not found."}), 404
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
 
-@app.route('/earthquakes/magnitude/<float:min_magnitude>')
-def earthquakes_by_magnitude(min_magnitude):
-    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= min_magnitude).all()
-    earthquakes_data = [quake.to_dict() for quake in earthquakes]
-    return jsonify({
-        "count": len(earthquakes_data),
-        "earthquakes": earthquakes_data
-    })
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    earthquakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquakes.append(earthquake.to_dict())
+    body = {'count': len(earthquakes),
+            'quakes':earthquakes
+            }
+    status = 200
+    return make_response(body, status)
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
